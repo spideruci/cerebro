526f3ddd5223b75a3cfac3ae7015d5a13bbb8148
VijayKrishna Palepu <vijay.0288@gmail.com>
2015-04-23 04:16:05 -0700

    Custom Automated Scalable Node Coloring - Now coloring nodes using HSL and some other smarts. - Made some minor UI fixes and adjustments. - Fixed a small but important bug in trace-replay-animation.

--- javascripts/thesteadybrain.js
+++ javascripts/thesteadybrain.js
@@ -1,4 +1,78 @@
-var color = d3.scale.category10();
+// var color = d3.scale.category10();
+
+
+var color_palete = function(number) {
+  var colors = [];
+  var jump = Math.ceil(360/number);
+  if(jump < 5) jump = 5;
+  for(var i = 0; i <= 360; i += jump) {
+    // var s = (colors.length % 2 === 0) ? 0.4 : 0.9;
+    var s = 1;
+    var x = d3.hsl(i, s, 0.5);
+    colors.push(x);
+    if(colors.length == number) break;
+  }
+
+  console.log(colors.length + " " + number);
+
+  if(colors.length < number) {
+    var new_colors = [];
+    var cycle_count = Math.floor(number / colors.length) - 1;
+    var residual_element_count = number % colors.length;
+
+    for(var i = 0; i < residual_element_count; i += 1) {
+      var rev_cyc_count = cycle_count + 1;
+      var color = colors[i];
+      new_colors.push(color);
+      var s = color.s; // should be 1.0
+      for(var j = 1; j <= 4 && j <= rev_cyc_count; j += 1) {
+        s = s - 0.1;
+        new_colors.push(d3.hsl(color.h, s, color.l));
+      }
+    }
+
+    for(var i = residual_element_count; i < colors.length; i += 1) {
+      var rev_cyc_count = cycle_count;
+      var color = colors[i];
+      new_colors.push(color);
+      var s = color.s; // should be 1.0
+      for(var j = 1; j <= 4 && j <= rev_cyc_count; j += 1) {
+        s = s - 0.1;
+        new_colors.push(d3.hsl(color.h, s, color.l));
+      }
+    }
+    colors = new_colors; 
+  }
+
+  console.log(colors.length);
+  
+  return colors;
+}
+
+var shuffle_colors = function(colors) {
+  var shuffled_colors = []
+  var mid = Math.floor(number / 2);
+  var odd = number % 2;
+  for(var i = 0; i < mid;  i += 1) {
+    shuffled_colors.push(colors[i]);
+    shuffled_colors.push(colors[i+ mid + odd]);
+  }
+  console.log(shuffled_colors.length);
+  if(odd === 1) {
+    shuffled_colors.push(colors[mid]);
+  }
+  return shuffled_colors;
+}
+
+
+var color_palete_hex = function(number) {
+  var x = color_palete(number);
+  return x.map(function(hsl) {
+    return hsl.toString();
+  });
+};
+
+
 
 var width = document.getElementsByTagName("body")[0].offsetWidth,
     height = 720;
@@ -9,9 +83,6 @@ var width = document.getElementsByTagName("body")[0].offsetWidth,
 var force = d3.layout.force().size([width, height]);
 
 var svg = d3.select("body").append("div").style("height", "80vh").append("svg")
-    // .attr("width", width)
-    // .attr("height", height)
-    // .style("background-color", "black")
     .attr("pointer-events", "all")
     .call(d3.behavior.zoom().on("zoom", redraw))
     ;
@@ -51,14 +122,51 @@ var method_dict = [];
 var maxDistance = 0;
 var max_wt = 1;
 var lasso;
+var all_classes = [];
+var color_picker;
 
-d3.json("brains/nanoxml3.json", function(error, graph) {
+var is_stop_class = function(class_name) {
+  if(class_name === null 
+      || class_name.length === 0
+      || class_name.toLowerCase().contains("test")) {
+    return true;
+  }
+  return false;
+}
+
+var stop_class_color = "#444"
+
+d3.json("brains/nanoxml.json", function(error, graph) {
   traces = graph.traces;
   traces_pointer = 0;
   ddg_nodes = graph.nodes;
   ddg_links = graph.links;
   var slider_element = document.getElementsByName("slider")[0];
   slider_element.max = traces[traces_pointer].size;
+
+  ddg_nodes.forEach(function(element) {
+    var class_name = element.className;
+    if(is_stop_class(class_name)) return;
+    if(all_classes.indexOf(class_name) === -1) {
+      all_classes.push(class_name);
+    }
+  });
+
+  console.log(all_classes.length);
+  all_classes = all_classes.sort();
+
+  ddg_nodes.forEach(function(element) {
+    var class_name = element.className;
+    if(is_stop_class(class_name)) {
+      element.colorGroup2 = all_classes.length;
+    } else {
+      element.colorGroup2 = all_classes.indexOf(class_name);
+    }
+  });
+
+  color_picker = d3.scale.ordinal()
+                  .domain(d3.range(all_classes.length))
+                  .range(color_palete_hex(all_classes.length));
 
   ddg_links.forEach(function(element) {
     var wt = element.value;
@@ -77,11 +185,6 @@ d3.json("brains/nanoxml3.json", function(error, graph) {
               .attr("content", function(d) {return d.value;})
               .style("stroke-width", function(d) { return plateau(d.value); })
               ;
-
-  
-
-  
-
 
 
   var toY = function(currentNode) { return currentNode.y }
@@ -118,16 +221,17 @@ d3.json("brains/nanoxml3.json", function(error, graph) {
 
   // Create the area where the lasso event can be triggered
   var lasso_area = vis.append("rect")
-                      .attr("x", nodes_x_min)
-                      .attr("y", nodes_y_min)
-                      .attr("width", nodes_x_max - nodes_x_min)
-                      .attr("height", nodes_y_max - nodes_y_min)
+                      .attr("x", nodes_x_min - 20)
+                      .attr("y", nodes_y_min - 20)
+                      .attr("width", nodes_x_max - nodes_x_min + 40)
+                      .attr("height", nodes_y_max - nodes_y_min + 40)
                       // .style("opacity",0);
                       .style("visibility", "visible")
                       .style("stroke", "pink")
                       .style("fill", "lightgrey")
                       .style("fill-opacity", 0.05)
                       .style("stroke-width", "2px");
+  lasso_area.append("title").text("Selection Mode. Panning disabled.");
 
   // Define the lasso
   lasso = d3.lasso()
@@ -199,15 +303,21 @@ d3.json("brains/nanoxml3.json", function(error, graph) {
   //   .style("fill-opacity", 0.05)
   //   .style("stroke-width", "2px");
 
-  d3.select("rect.background").append("title").text("Selection Mode")
+  // d3.select("rect.background").append("title").text("Selection Mode")
   
   var node = vis.selectAll(".node")
               .data(graph.nodes)
               .enter().append("circle")
               .attr("class", "node")
               .attr("r", 5)
-              .style("fill", function(d) { return color(d.colorGroup); })
-              .attr("content", function(d) { return get_instruction_info(d); });
+              .style("fill", function(d) {
+                if(d.colorGroup2 === all_classes.length) 
+                  return stop_class_color;
+                return color_picker(d.colorGroup2); 
+              })
+              .attr("content", function(d) { 
+                return get_instruction_info(d); 
+              });
 
   var exec_options = d3.select("select").selectAll("option")
                         .data(graph.traces)
@@ -219,7 +329,7 @@ d3.json("brains/nanoxml3.json", function(error, graph) {
   
 
   node.append("title")
-      .text(function(d) { return get_instruction_info(d); });
+      .text(function(d) { return d.id + "\n" + get_instruction_info(d); });
 
   lasso.items(d3.selectAll(".node"));
 
@@ -247,11 +357,24 @@ var magic = function(links, node, max_width, max_height) {
       time = end - start;
       console.log(time);
       time = time / 1000;
-      document.getElementsByName("state")[0].value = "loading completed in ... "
-      document.getElementById("timeview").innerHTML = time.toString() + " seconds."
+      // document.getElementsByName("state")[0].value = "loading completed in ... "
+      // document.getElementById("timeview").innerHTML = time.toString() + " seconds."
       for(var i = 0; i < document.getElementsByTagName("button").length; i += 1) {
         document.getElementsByTagName("button")[i].disabled = false  
       }
+}
+
+var playpause = function() {
+  var playpause_span = document.getElementsByName("playpause")[0];
+  var playpause_class = playpause_span.getAttribute("class")
+  if(playpause_class === "glyphicon glyphicon-play") {
+    playpause_span.setAttribute("class", "glyphicon glyphicon-pause")  
+    resume();
+  } else if(playpause_class === "glyphicon glyphicon-pause") {
+    playpause_span.setAttribute("class", "glyphicon glyphicon-play")  
+    pause();
+  }
+  
 }
 
 var pause = function() {
@@ -268,14 +391,16 @@ var resume = function() {
   slider.step = 1; 
 }
 
-var slider_delay = 1;
+var slider_delay;
 var timerCallBack = function() {
   var slider = document.getElementsByName("slider")[0];
   var length = traces[traces_pointer].size;
   var count = parseInt(slider.value);
   var step = parseInt(slider.step);
-  slider.value = count + step;
-  slider.onchange();
+  if(step != 0) {
+    slider.onchange();
+    slider.value = count + step;
+  }
   
   if(slider.value < length) {
     setTimeout(timerCallBack, slider_delay);
@@ -283,6 +408,9 @@ var timerCallBack = function() {
 }
 
 var blink3 = function() {
+  var begin_execution_span = document.getElementsByName("begin_execution")[0];
+  begin_execution_span.setAttribute("class", "glyphicon glyphicon-repeat");
+
   var playstate = document.getElementsByName("playstate")[0];
   playstate.innerHTML = "Fetching Trace";
   var trace_name = traces[traces_pointer].name;
@@ -292,10 +420,11 @@ var blink3 = function() {
     trace = json;
     playstate.innerHTML = "Playing";
     var slider = document.getElementsByName("slider")[0];
-    slider.value = 1;
+    slider.max = traces[traces_pointer].size;
+    slider.value = 0;
+    document.getElementsByName("slowdown")[0].onchange();
     timerCallBack();
   });
-  
 }
 
 var slowdown = function(value) {
@@ -311,7 +440,9 @@ var slide = function(value) {
   var playback_delay = 10;
   var counter = value;
   var counter2 = 0;
-  node = nodes[0][trace[counter]]
+  node = nodes[0][trace[counter] - 1]
+  // console.log(node.__data__.id === trace[counter])
+  // console.log(node.__data__.id + " " + trace[counter] + " " + counter);
   t00 = d3.select(node).transition().duration(10);
   t00.delay(function(d, j) { return (50) + (playback_delay); })
   .each("end", function() {
@@ -328,7 +459,7 @@ var slide = function(value) {
       document.getElementsByTagName("button")[3].disabled = false;
     }
   })
-  .attr("r", "10")
+  .attr("r", "12.5")
   .style("fill", "white");
 
   var t11 = t00.transition().duration(10);
@@ -337,7 +468,11 @@ var slide = function(value) {
 
   })
   .attr("r", "5")
-  .style("fill", function(d) { return color(d.colorGroup); });
+  .style("fill", 
+    function(d) { 
+      if(d.colorGroup2 === all_classes.length) return stop_class_color; 
+      return color_picker(d.colorGroup2); 
+    });
 }
 
 var get_instruction_info = function(insn) {
@@ -351,48 +486,52 @@ var get_instruction_info = function(insn) {
 
 var toggle_edges = function() {
   if(vis.select(".link").style("visibility") === "visible") {
-    vis.selectAll(".link")
-      .attr("visibility", "hidden");  
+    vis.selectAll(".link").attr("visibility", "hidden");  
   } else {
-    vis.selectAll(".link")
-      .attr("visibility", "visible");  
+    vis.selectAll(".link").attr("visibility", "visible");  
   } 
 }
 
-var remove_edge_weights = function() {
-  var links = vis.selectAll(".link")[0];
-  for(var i = 0 ; i < links.length; i += 1) {
-    links[i].style.strokeWidth = 2 + "px";
+var toggle_edge_weights = function(btn) {
+  var weights = btn.getAttribute("weights");
+  if(weights === "0") {
+    add_edge_weights();
+    btn.setAttribute("weights", "1");
+  } else if(weights === "1") {
+    remove_edge_weights();
+    btn.setAttribute("weights", "0")
   }
 }
 
-var add_edge_weights = function() {
-  var links = vis.selectAll(".link")[0];
-  var max = -1;
-  for(var i = 0 ; i < links.length; i += 1) {
-    var width =parseInt(links[i].getAttribute("content"));
-    if(width > max) max = width;
-    links[i].style.strokeWidth = plateau(width)  + "px";
+  var remove_edge_weights = function() {
+    var links = vis.selectAll(".link")[0];
+    for(var i = 0 ; i < links.length; i += 1) {
+      links[i].style.strokeWidth = 2 + "px";
+    }
   }
-  console.log(max);
-}
 
-var plateau = function(value) {
-  var r = 50;
-  var x = r * (value/max_wt);
-  var y = Math.sqrt((x*x) + (x*r*2))
-  return y;
-}
+  var add_edge_weights = function() {
+    var links = vis.selectAll(".link")[0];
+    var max = -1;
+    for(var i = 0 ; i < links.length; i += 1) {
+      var width =parseInt(links[i].getAttribute("content"));
+      if(width > max) max = width;
+      links[i].style.strokeWidth = plateau(width)  + "px";
+    }
+  }
+
+    var plateau = function(value) {
+      var r = 50;
+      var x = r * (value/max_wt);
+      var y = Math.sqrt((x*x) + (x*r*2))
+      return y;
+    }
 
 var change_execution = function(value) {
+  var begin_execution_span = document.getElementsByName("begin_execution")[0];
+  begin_execution_span.setAttribute("class", "glyphicon glyphicon-off");
   document.getElementsByTagName("button")[3].disabled = false;
-  console.log(value)
   traces_pointer = value;
-  console.log(traces_pointer)
-  var slider_element = document.getElementsByName("slider")[0];
-  slider_element.max = traces[traces_pointer].size;
-  slider_element.value = slider_element.min;
-  trace = null;
 }
 
 var lock_transformation = function(checkbox) {
@@ -498,22 +637,31 @@ var lasso_end = function() {
 };
 
 var activate_selection = function(selected_nodes) {
+  var clean_type_desc = function(name) {
+    var re = new RegExp("L[a-z0-9]+/", "g");
+    var re2 = new RegExp("[a-z0-9]+/", "g");
+    return name.replace(re, "", "g").replace(re2, "", "g");
+  }
+
   dict = [];
   method_dict = [];
   
-  console.log(selected_nodes);
   for (var count = 0; count < selected_nodes.length; count += 1) {
     var temp = selected_nodes[count].getAttribute("content");
     put_classname_in_dict(temp);
     put_methodname_in_dict(temp);
   }
 
+  dict = dict.sort(function (a, b) {
+    return parseInt(b.value) - parseInt(a.value);
+  });
+
   var output = "";
-  if(dict.length != 0) output = "<b>Classes Selected:</b> <br/>";
+  if(dict.length != 0) output = "<b>Selection's Class Distribution</b> <br/>";
   for (var count2 = 0; count2 < dict.length; count2 += 1) {
     var classCount = dict[count2].value
     var count_markup = "<span class=\"badge\">" + classCount + "</span>";
-    var className = dict[count2].key.substring("Class: ".length);
+    var className = dict[count2].key.substring("Class: ".length).replace(",", "");
     var temp = count_markup + " " + className + "<br/>";
 
     output += temp;
@@ -521,10 +669,17 @@ var activate_selection = function(selected_nodes) {
 
   document.getElementById("classlist").innerHTML = output;
 
+  method_dict = method_dict.sort(function (a, b) {
+    return parseInt(b.value) - parseInt(a.value);
+  });
+
   var output2 = "";
-  if(method_dict.length != 0) output2 = "<b>Methods Selected:</b> <br/>";
+  
+  if(method_dict.length != 0) output2 = "<b>Selection's Method Distribution</b> <br/>";
   for (var count2 = 0; count2 < method_dict.length; count2 += 1) {
     var methodName = method_dict[count2].key.substring("Method: ".length);
+    methodName = methodName.replace("<", "&lt;", "g").replace(">", "&gt;", "g");
+    methodName = clean_type_desc(methodName).replace(",", "");
     var methodCount = "<span class=\"badge\">" + method_dict[count2].value + "</span>";
     var temp = methodCount + " " + methodName + "<br/>";
     output2 += temp;
